86816b9c892f76102b00433135268888543af51c
FPC25 <felipepcorral@gmail.com>
2025-06-30 20:46:53 -0300

    Implement grade calculator functionality in Exercise2 Project

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,56 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+
+        float grade = int.Parse(input);
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string sign = "";
+        if (letter != "A" || letter != "F")
+        {
+            if (grade % 10 > 6)
+            {
+                sign = "+";
+            }
+            else if (grade % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed with a grade of {letter}{sign}.");
+        }
+        else
+        {
+            Console.WriteLine($"Unfortunately, you failed with a grade of {letter}{sign}.");
+        }
+
+        Console.WriteLine("Thank you for using the grade calculator!");
+        Console.WriteLine("Goodbye!");
     }
 }
