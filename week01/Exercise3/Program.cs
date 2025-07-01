60fb1da8d36b6c8a695f1d7a45a93c9fb20a35eb
FPC25 <felipepcorral@gmail.com>
2025-06-30 22:08:40 -0300

    Completed the guessing game with 3 different levels of difficulty and failsafe against the user

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,84 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 3 Project.");
+
+        Random randomGenerator = new Random();
+
+        string response;
+        string input;
+        do
+        {
+            bool isNumeric = false;
+            int level = -1;
+
+            do
+            {
+                Console.Write("Choose a number from 1 to 3 to select the difficulty level (default is 2. Medium): \n1. Easy (max value = 50)\n2. Medium (max value = 100)\n3. Hard (max value = 1000)\nLevel: ");
+                input = Console.ReadLine();
+                isNumeric = int.TryParse(input, out level);
+                Console.WriteLine("");
+            } while (!isNumeric || level < 1 || level > 3);
+
+            int maxNumber;
+            if (level == 1)
+            {
+                maxNumber = 50;
+            }
+            else if (level == 3)
+            {
+                maxNumber = 1000;
+            }
+            else
+            {
+                maxNumber = 100;
+            }
+
+            int number = randomGenerator.Next(1, maxNumber + 1);
+            int guess = -1;
+            int numGuesses = 0;
+            while (guess != number)
+            {
+                isNumeric = false;
+                do
+                {
+                    Console.Write("What is your guess? ");
+                    input = Console.ReadLine();
+                    isNumeric = int.TryParse(input, out guess);
+                } while (!isNumeric);
+
+                numGuesses++;
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {numGuesses} guesses!\n");
+                }
+            }
+
+            do
+            {
+                Console.WriteLine("Do you want to continue (y/n)?");
+                input = Console.ReadLine();
+                Console.WriteLine("");
+            } while (input == "");
+
+            response = input.ToLower()[0].ToString();
+            if (response != "y" && response != "n")
+            {
+                response = "n";
+            }
+
+        } while (response == "y");
     }
 }
