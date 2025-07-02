fcf5aa373c24a79c0ee4b08bfc8b881976882ebe
FPC25 <felipepcorral@gmail.com>
2025-07-02 20:49:01 -0300

    Refactor Exercise 4 Project to add core requirements

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 4 Project.");
+
+        List<float> numbers = new List<float>();
+        float number, sum = 0, average, large = 0;
+        bool isNumeric;
+        string input;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            isNumeric = false;
+            do
+            {
+                Console.Write("Enter a number: ");
+                input = Console.ReadLine();
+                isNumeric = float.TryParse(input, out number);
+
+                if (!isNumeric)
+                {
+                    Console.WriteLine($"Sorry, but {input} is not a number.");
+                }
+            } while (!isNumeric);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+
+            }
+        } while (number != 0);
+
+        foreach (float num in numbers)
+        {
+            sum += num;
+            if (num > large)
+            {
+                large = num;
+            }
+        }
+        average = sum / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}\nThe average is: {average}\nThe largest number is: {large}");
     }
 }
