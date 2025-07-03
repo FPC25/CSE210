b912f9c8def99a2f4f6f13345470a5073fcab1de
FPC25 <felipepcorral@gmail.com>
2025-07-03 11:49:17 -0300

    Implement user interaction features: welcome message, name prompt, favorite number input, and result display

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,53 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        string input;
+        bool isNumeric;
+        int number;
+        do
+        {
+            Console.Write("What is your Favorite number? ");
+            input = Console.ReadLine();
+            isNumeric = int.TryParse(input, out number);
+
+            if (!isNumeric)
+            {
+                Console.WriteLine($"Sorry, but {input} is not a integer number.");
+            }
+        } while (!isNumeric);
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squareNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squareNumber}");
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
+        
     }
 }
