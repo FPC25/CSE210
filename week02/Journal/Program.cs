fe4c255ad177680c581012b9ad8ed1594f072f4e
FPC25 <felipepcorral@gmail.com>
2025-07-04 17:24:46 -0300

    Implement prompt selection and entry display in Main method

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        string pathToPrompts = "./prompts.json";
+        var promptObg = new PromptGuide(pathToPrompts);
+
+        var newEntry = new Entry(promptObg.SelectPrompt());
+        newEntry.Display();
+
     }
 }
