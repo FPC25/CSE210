d659c53dc5ec60b882443b87873251b1a84ee4a5
FPC25 <felipepcorral@gmail.com>
2025-07-04 12:58:48 -0300

    Refactor Job constructor to accept parameters for company, job title, start year, and end year

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -3,8 +3,12 @@ public class Job
 {
     public string _company, _jobTitle;
     public int _startYear, _endYear;
-    public Job()
+    public Job(string company, string jobTitle, int startYear, int endYear)
     {
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
     }
 
     public void Display()
