7a2ed7d5a482659402eb1e362b708181b17295a4
FPC25 <felipepcorral@gmail.com>
2025-07-04 18:20:59 -0300

    Remove unused import statement and comment in Program.cs

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,10 +1,13 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        //string prefsPath = "./preferences.json";
 
         string pathToPrompts = "./prompts.json";
         var promptObg = new PromptGuide(pathToPrompts);
