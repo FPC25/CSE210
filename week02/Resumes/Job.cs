7b22e7ac46d9eec8cc4b0dd58186188a8747f5e1
FPC25 <felipepcorral@gmail.com>
2025-07-04 12:30:50 -0300

    Refactor Job class to include properties and display method for job information

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,8 +1,14 @@
 using System;
 public class Job
 {
-    public 
+    public string _company, _jobTitle;
+    public int _startYear, _endYear;
     public Job()
     {
+    }
+
+    public void DisplayJobInfo()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
