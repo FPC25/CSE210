38dad6c3eb89c8c7a653e7a5ddb8607cd302a287
FPC25 <felipepcorral@gmail.com>
2025-07-04 13:05:33 -0300

    Add job listings and display functionality in Program class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        List<Job> jobs = new List<Job>();
+
+        jobs.Add(new Job("Microsoft", "Software Engineer", 2019, 2022));
+
+        jobs.Add(new Job("Apple", "Manager", 2022, 2023));
+
+        Resume employeeResume = new Resume("Allison Rose", jobs);
+
+        employeeResume.Display();
     }
 }
