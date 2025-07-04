186a22b1ed771aec1ee01d043fbfed0f86ed2b63
FPC25 <felipepcorral@gmail.com>
2025-07-04 19:31:23 -0300

    Add comments to clarify preferences configuration in Program.cs

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -7,8 +7,10 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
 
+        // Change the preferences config file if needed
         string prefsPath = "./JournalConfig.json";
 
+        // Don't touch nothing under here
         var prefs = new Preferences(prefsPath);
 
         Console.WriteLine(prefs._userName);
