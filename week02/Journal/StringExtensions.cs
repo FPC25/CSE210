6baec993ae930bbd7913a69353937b05026e5dfc
FPC25 <felipepcorral@gmail.com>
2025-07-04 19:55:20 -0300

    Move CapitalizeFirst and Decision methods to Utils.cs for better organization

--- week02/Journal/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public static class StringExtensions
-{
-    public static string CapitalizeFirst(this string s)
-    {
-        if (string.IsNullOrEmpty(s)) return s;
-        return char.ToUpper(s[0]) + s.Substring(1);
-    }
-}
---
+++ week02/Journal/Utils.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+public static class Utils
+{
+    public static string CapitalizeFirst(this string s)
+    {
+        if (string.IsNullOrEmpty(s)) return s;
+        return char.ToUpper(s[0]) + s.Substring(1);
+    }
+
+    public static int Decision(List<string> options)
+    {
+        // Display numbered choices
+        for (int i = 0; i < options.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {options[i]}");
+        }
+
+        int choice;
+        string input;
+        // Prompt until user enters a valid number
+        do
+        {
+            Console.Write("Please select an option by number: ");
+            input = Console.ReadLine();
+        } while (!int.TryParse(input, out choice) || choice < 1 || choice > options.Count);
+
+        // Return zero-based index
+        return choice - 1;
+    }
+}
