c0f159f5e39538882d1d989c9fb704d30cb7c7eb
FPC25 <felipepcorral@gmail.com>
2025-07-05 11:37:26 -0300

    Implement Journal class with menu options for loading, writing, displaying, and saving entries

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,6 +1,106 @@
 using System;
+using System.Text.Json;
 
 class Journal
 {
-    
+    public string _userName, _preferredExtension, _dateFormat;
+    public List<Entry> _entries;
+    private List<string> _options;
+
+    public Journal(string name, string extension, string dateFormat)
+    {
+        _userName = name;
+        _preferredExtension = extension;
+        _dateFormat = dateFormat;
+    }
+
+    public void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine($"Hello {_userName}! Please Select one of the following options: ");
+        _options = new List<string>() { "Load a Journal", "Write a new entry", "Display the Journal", "Save Journal", "Quit" };
+
+        string selectedOption = Utils.Decision(_options);
+
+        if (selectedOption == "Load a Journal")
+        {
+            Console.WriteLine($"I see {_userName} that you want to load your journal!");
+            string fileName;
+            do
+            {
+                Console.Write($"What is the name of your file with the extension {_preferredExtension}?");
+                fileName = Console.ReadLine();
+            } while (!FindFile(fileName));
+
+            _entries = LoadJournal(fileName);
+            Menu();
+        }
+        else if (selectedOption == "Write a new entry")
+        {
+            var promptObg = new PromptGuide("./prompts.json");
+            Entry newEntry = new Entry(promptObg.SelectPrompt(), _dateFormat);
+            newEntry.Display();
+            Console.WriteLine($"What do you want to do with your new Journal Entry?");
+            _options = new List<string>() { "Save", "Edit", "Discard" };
+
+            string entryOption = Utils.Decision(_options);
+
+            if (entryOption == "Save")
+            {
+                SaveEntry();
+            }
+            else if (entryOption == "Edit")
+            {
+                EditEntry();
+            }
+            else
+            {
+                Menu();
+            }
+
+        }
+        else if (selectedOption == "Display the Journal")
+        {
+            // Display journal logic here
+        }
+        else if (selectedOption == "Save Journal")
+        {
+            // Save journal logic here
+        }
+        else
+        {
+            Environment.Exit(0);
+        }
+    }
+
+    public bool FindFile(string fileName)
+    {
+
+    }
+
+    public List<Entry> LoadJournal(string fileName)
+    {
+
+    }
+
+    public Entry SaveEntry()
+    {
+
+    }
+
+    public Entry EditEntry()
+    {
+
+    }
+
+    public void SaveJournal()
+    {
+
+    }
+
+    public void DisplayJournal()
+    {
+
+    }
+
 }
