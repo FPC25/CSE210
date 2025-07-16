54b95cd8fa67de47c906ea67eb115228ef3b2099
FPC25 <felipepcorral@gmail.com>
2025-07-16 17:33:14 -0300

    Created the Class Fractions with 3 constructors, setters and getters to the numerator and denominator and created methods to represents the fractions values externally

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,54 @@
+using System;
+
+class Fraction
+{
+    private int _top, _bottom;
+    public Fraction()
+    {
+        this.SetTop(1);
+        this.SetBottom(1);
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        this.SetTop(wholeNumber);
+        this.SetBottom(1);
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        this.SetTop(top);
+        this.SetBottom(bottom);
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{this.GetTop()}/{this.GetBottom()}"; 
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)this.GetTop() / this.GetBottom(); 
+    }
+    
+}
