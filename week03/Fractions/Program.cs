5f2812252e353a29f9cdc66cb8fd205e685b792d
FPC25 <felipepcorral@gmail.com>
2025-07-16 17:45:00 -0300

    Simplify initialization of Fraction objects in Program.cs to initialize inside the list of numbers

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -6,12 +6,7 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
 
-        Fraction number1 = new Fraction();
-        Fraction number2 = new Fraction(5);
-        Fraction number3 = new Fraction(3, 4);
-        Fraction number4 = new Fraction(1, 3);
-
-        List<Fraction> numbers = new List<Fraction> { number1, number2, number3, number4 };
+        List<Fraction> numbers = new List<Fraction> { new Fraction(), new Fraction(5), new Fraction(3, 4), new Fraction(1, 3) };
 
         foreach (Fraction number in numbers)
         {
