4f52862adf8925cf4044beab6ed7a76b70cf4b74
FPC25 <felipepcorral@gmail.com>
2025-07-16 17:43:15 -0300

    Add Fraction objects testing the different types of constructors  for Fraction Class and tested the display methods

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction number1 = new Fraction();
+        Fraction number2 = new Fraction(5);
+        Fraction number3 = new Fraction(3, 4);
+        Fraction number4 = new Fraction(1, 3);
+
+        List<Fraction> numbers = new List<Fraction> { number1, number2, number3, number4 };
+
+        foreach (Fraction number in numbers)
+        {
+            Console.WriteLine(number.GetFractionString());
+            Console.WriteLine(number.GetDecimalValue());
+        }
     }
 }
