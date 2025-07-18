ca866b531ed80b05e9d258e3d4290b6474cc0039
FPC25 <felipepcorral@gmail.com>
2025-07-18 17:31:10 -0300

    Add a method to convert to a text to a list of Word objects

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -8,18 +8,24 @@ class Scripture
     public Scripture(Reference reference, string text)
     {
         _reference = reference;
-        List<string> list = text.Split(' ').ToList();
-        foreach (string item in list)
-        {
-
-        }
+        _scripture = ConvertingStringToWord(SplittingText(text));
     }
 
-    private List<string> SplittingText(string text)
+    static private List<string> SplittingText(string text)
     {
-    // Split on word boundaries, keeping both words and punctuation
-    return Regex.Split(text, @"(\W+)")
-        .Where(s => !string.IsNullOrWhiteSpace(s))
-        .ToList();
+        // Split on word boundaries, keeping both words and punctuation
+        return Regex.Split(text, @"(\W+)")
+            .Where(s => !string.IsNullOrWhiteSpace(s))
+            .ToList();
+    }
+
+    static private List<Word> ConvertingStringToWord(List<string> initialList)
+    {
+        List<Word> finalList = new List<Word> { };
+        foreach (string item in initialList)
+        {
+            finalList.Add(new Word(item));
+        }
+        return finalList;
     }
 }
