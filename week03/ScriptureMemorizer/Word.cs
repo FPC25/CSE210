da03fa71a297a1cc2c81a2aa657371e94f9eab0b
FPC25 <felipepcorral@gmail.com>
2025-07-18 15:52:14 -0300

    Add Word class with methods for hiding and displaying words

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -2,5 +2,40 @@ using System;
 
 class Word
 {
-    
+    private string _text;
+    private bool _isHidden = false;
+
+    public Word(string text)
+    {
+        _text = text;
+    }
+
+    public void HideWord()
+    {
+        if (!this.IsHidden())
+        {
+            _text = new string('_', _text.Length);
+            this.Hide();
+        }
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    private void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public string GetWord()
+    {
+        return _text;
+    }
+
+    public void Display()
+    {
+        Console.Write(_text);
+    }
 }
