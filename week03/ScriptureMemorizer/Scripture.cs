cfed245dee0b22a703eb76ad1fc40516920fae6c
FPC25 <felipepcorral@gmail.com>
2025-07-18 17:30:20 -0300

    Refactor Scripture class to initialize _scripture and _reference, and add a new method for splitting text

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,5 +1,25 @@
 using System;
-
+using System.Text.RegularExpressions;
 class Scripture
 {
+    private List<Word> _scripture;
+    private Reference _reference;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        List<string> list = text.Split(' ').ToList();
+        foreach (string item in list)
+        {
+
+        }
+    }
+
+    private List<string> SplittingText(string text)
+    {
+    // Split on word boundaries, keeping both words and punctuation
+    return Regex.Split(text, @"(\W+)")
+        .Where(s => !string.IsNullOrWhiteSpace(s))
+        .ToList();
+    }
 }
