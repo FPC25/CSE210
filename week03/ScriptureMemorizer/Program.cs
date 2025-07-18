15f51debc157b65a7ea260339485edbc994b86f3
FPC25 <felipepcorral@gmail.com>
2025-07-18 19:19:54 -0300

    Add initial setup for ScriptureMemorizer project with sample scripture display

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding; In all thy ways acknowledge him, and he shall direct thy paths.";
+
+        Scripture test = new Scripture(new Reference("Proverbs", 3, 5, 6), text);
+        test.Display();
     }
 }
