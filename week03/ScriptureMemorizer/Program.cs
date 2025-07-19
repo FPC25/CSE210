21feec03a6fbc5dc6032ae19dabe9621d932df8d
FPC25 <felipepcorral@gmail.com>
2025-07-18 22:22:06 -0300

    Refactor Program class to improve structure and enhance readability by organizing methods

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -9,6 +9,47 @@ class Program
         string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding; In all thy ways acknowledge him, and he shall direct thy paths.";
 
         Scripture test = new Scripture(new Reference("Proverbs", 3, 5, 6), text);
-        test.Display();
+
+        Run(test);
     }
+
+    static void DisplayMessage(Scripture scripture, string text)
+    {
+        scripture.Display();
+        Console.WriteLine();
+        Console.WriteLine(text);
+    }
+
+    static void Run(Scripture scripture)
+    {
+        string endLine = "Press enter to continue or type 'quit' to finish:";
+        int random_amount = 3;
+
+        string line;
+
+        while (true)
+        {
+            Console.Clear();
+            DisplayMessage(scripture, endLine);
+
+            line = Console.ReadLine();
+
+            if (line.Equals("quit", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+
+            for (int i = 0; i < random_amount; i++)
+            {
+                scripture.HideRandomWord();
+            }
+            
+
+            if (scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+        }
+    }
+
 }
