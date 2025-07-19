f91d02eff44b538b3d10a2c0ddc604bb239a75e3
FPC25 <felipepcorral@gmail.com>
2025-07-18 23:52:17 -0300

    Add Chapter class with properties for chapter number, reference, and verses management

---
+++ week03/ScriptureMemorizer/Chapter.cs
@@ -0,0 +1,60 @@
+using System;
+
+class Chapter
+{
+    private int _chapter;
+    private string _reference;
+    private List<Verse> _verses;
+
+    public Chapter()
+    {
+        _verses = new List<Verse>();
+    }
+
+    public int GetChapter()
+    {
+        return _chapter;
+    }
+
+    public void SetChapter(int chapter)
+    {
+        _chapter = chapter;
+    }
+
+    public string GetReference()
+    {
+        return _reference;
+    }
+
+    public void SetReference(string reference)
+    {
+        _reference = reference;
+    }
+
+    public List<Verse> GetVerses()
+    {
+        return _verses;
+    }
+
+    public void SetVerses(List<Verse> verses)
+    {
+        _verses = verses;
+    }
+
+    public Verse GetVerse(int verseNumber)
+    {
+        foreach (Verse verse in _verses)
+        {
+            if (verse.GetVerse() == verseNumber)
+            {
+                return verse;
+            }
+        }
+        return null;
+    }
+
+    public int GetVerseCount()
+    {
+        return _verses.Count;
+    }
+}
