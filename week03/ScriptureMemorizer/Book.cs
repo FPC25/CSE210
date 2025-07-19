185d03acd2ade999d3374f8f777fedf367b05ee9
FPC25 <felipepcorral@gmail.com>
2025-07-19 00:05:37 -0300

    Add Book class with properties for book name and chapters management

---
+++ week03/ScriptureMemorizer/Book.cs
@@ -0,0 +1,49 @@
+using System;
+
+class Book
+{
+    private string _book;
+    private List<Chapter> _chapters;
+
+    public Book()
+    {
+        _chapters = new List<Chapter>();
+    }
+
+    public string GetBook()
+    {
+        return _book;
+    }
+
+    public void SetBook(string book)
+    {
+        _book = book;
+    }
+
+    public List<Chapter> GetChapters()
+    {
+        return _chapters;
+    }
+
+    public void SetChapters(List<Chapter> chapters)
+    {
+        _chapters = chapters;
+    }
+
+    public Chapter GetChapter(int chapterNumber)
+    {
+        foreach (Chapter chapter in _chapters)
+        {
+            if (chapter.GetChapter() == chapterNumber)
+            {
+                return chapter;
+            }
+        }
+        return null; // Chapter not found
+    }
+
+    public int GetChapterCount()
+    {
+        return _chapters.Count;
+    }
+}
