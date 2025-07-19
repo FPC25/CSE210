9e648916260479d42aabbb5f19794bea6dc0945a
FPC25 <felipepcorral@gmail.com>
2025-07-19 11:08:15 -0300

    Implemented Getters for Scripture Collection that may access the books and chapters and verses from a collection as BOM or the new testament or simply the amount of books in it.

--- week03/ScriptureMemorizer/ScriptureCollection.cs
+++ week03/ScriptureMemorizer/ScriptureCollection.cs
@@ -6,8 +6,33 @@ class ScriptureCollection
 
     public ScriptureCollection()
     {
+        _books = new List<Book>();
+    }
+
+    public void LoadFromFile(string filePath)
+    {
 
     }
 
+    public Book GetBook(string bookName)
+    {
+        foreach (Book book in _books)
+        {
+            if (book.GetBook().Equals(bookName, StringComparison.OrdinalIgnoreCase))
+            {
+                return book;
+            }
+        }
+        return null;
+    }
 
+    public List<Book> GetAllBooks()
+    {
+        return _books;
+    }
+
+    public int GetBookCount()
+    {
+        return _books.Count;
+    }
 }
