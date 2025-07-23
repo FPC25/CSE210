2afbc3904c28b7ab1bf7bc72145a072e8c916a32
FPC25 <felipepcorral@gmail.com>
2025-07-23 19:09:01 -0300

    Add Utils class with utility methods for string manipulation and list printing

---
+++ week04/OnlineOrdering/Utils.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+public static class Utils
+{
+    public static string CapitalizeFirst(this string s)
+    {
+        if (string.IsNullOrEmpty(s)) return s;
+        return char.ToUpper(s[0]) + s.Substring(1);
+    }
+
+    public static string Decision(List<string> options)
+    {
+        // Display numbered choices
+        for (int i = 0; i < options.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {options[i]}");
+        }
+
+        int choice;
+        string input;
+        // Prompt until user enters a valid number
+        do
+        {
+            Console.Write("Please select an option by number: ");
+            input = Console.ReadLine();
+        } while (!int.TryParse(input, out choice) || choice < 1 || choice > options.Count);
+
+        // Return zero-based index
+        return options[choice - 1];
+    }
+
+    public static void PrintList(List<string> list)
+    {
+        Console.Write("[");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write($"\"{list[i]}\"");
+            if (i < list.Count - 1)
+                Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+
+    internal static void PrintProductList(List<Product> list)
+    {
+        Console.Write("[");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write($"\"{list[i].GetProductName()}\"");
+            if (i < list.Count - 1)
+                Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+}
