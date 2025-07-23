d1c453cfcd7d4587557608f01477a9598ebd95dc
FPC25 <felipepcorral@gmail.com>
2025-07-23 17:15:36 -0300

    Refactor Customer class to implement constructor and methods for name and address handling

--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -2,13 +2,26 @@ using System;
 
 class Customer
 {
-    private int varName;
-    public Customer()
+    private string _name;
+    private Address _userAddress;
+    public Customer(string name, Address userAddress)
     {
+        _name = name;
+        _userAddress = userAddress;
     }
 
-    public void MethodName()
+    public bool IsFromUS()
     {
+        return _userAddress.IsExportation();
+    }
 
+    public string CustomerName()
+    {
+        return _name;
+    }
+
+    public string CustomerAddress()
+    {
+        return _userAddress.FullAddress();
     }
 }
