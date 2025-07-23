3fb0f1b5756291d4c8a3298af233a3b15eac7789
FPC25 <felipepcorral@gmail.com>
2025-07-23 17:08:13 -0300

    Refactor Address class to implement constructor and methods for address handling

--- week04/OnlineOrdering/Address.cs
+++ week04/OnlineOrdering/Address.cs
@@ -2,13 +2,26 @@ using System;
 
 class Address
 {
-    private int varName;
-    public Address()
+    private string _street, _city, _state, _country;
+    public Address(string street, string city, string state, string country)
     {
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
     }
 
-    public void MethodName()
+    public bool IsExportation()
     {
+        if (_country.Equals("USA") || _country.Equals("US") || _country.Equals("United States of America"))
+        {
+            return true;
+        }
+        return false;
+    }
 
+    public string FullAddress()
+    {
+        return $"{_street}, {_city}, {_state}, {_country}";
     }
 }
