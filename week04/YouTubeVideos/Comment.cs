d10a3f43545ef39a01b1c200b1a8d65a797e9586
FPC25 <felipepcorral@gmail.com>
2025-07-25 12:34:38 -0300

    Add initial implementation of Comment and Video classes

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Comment
+{
+    private int varName;
+    public Comment(string name, string text)
+    {
+    }
+
+    public void MethodName()
+    {
+
+    }
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Video
+{
+    private int varName;
+    Video(string title, string author, int length, List<Comment> comments = null)
+    {
+    }
+
+    public void MethodName()
+    {
+
+    }
+}
