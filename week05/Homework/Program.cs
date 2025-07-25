ca7ad1d121b7a57c9d6d6104979db2623e6ebeaf
FPC25 <felipepcorral@gmail.com>
2025-07-25 18:09:12 -0300

    Refactor Main method in Program class to improve readability and structure

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment hw1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(hw1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment hw2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(hw2.GetHomeworkList());
+
+        WritingAssignment hw3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(hw3.GetWritingInfo());
     }
 }
