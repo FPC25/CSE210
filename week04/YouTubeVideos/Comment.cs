22206cea65661918f5b912f660ef657a40f62f07
FPC25 <felipepcorral@gmail.com>
2025-07-25 12:40:59 -0300

    Refactor Comment class to use private fields for name and text, and update method names for clarity

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -2,13 +2,20 @@ using System;
 
 class Comment
 {
-    private int varName;
+    private string _name, _text;
     public Comment(string name, string text)
     {
+        _name = name;
+        _text = text;
     }
 
-    public void MethodName()
+    public string GetCommenterName()
     {
+        return _name;
+    }
 
+    public string GetCommentText()
+    {
+        return _text;
     }
 }
