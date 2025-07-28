9ba88dfd385b3aebb13ab3f4b6224317cd449206
FPC25 <felipepcorral@gmail.com>
2025-07-28 14:44:00 -0300

    Enhance BreathingActivity class with console prompts for breathing instructions

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -16,13 +16,21 @@ class BreathingActivity : Activity
     {
         List<string> breathIn = new List<string> { " ", ".", "o", "O" };
 
+        Console.Write($"Breathe in for {timeInSeconds} seconds: ");
         Utils.RepeatListString(breathIn, timeInSeconds);
     }
-    
+
     public static void BreatheOut(int timeInSeconds)
     {
-        List<string> breathIn = new List<string> { "O", "o", ".", " " }; 
+        List<string> breathIn = new List<string> { "O", "o", ".", " " };
 
+        Console.Write($"Breathe out for {timeInSeconds} seconds: ");
         Utils.RepeatListString(breathIn, timeInSeconds);
+    }
+
+    public void Hold(int timeInSeconds)
+    {
+        Console.Write($"Hold for {timeInSeconds} seconds:");
+        Countdown(timeInSeconds);
     }
 }
