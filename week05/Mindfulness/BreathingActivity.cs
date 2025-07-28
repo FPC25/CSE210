17ed037a3e4b9af91e25b09d8ad5ef8ae7a16e02
FPC25 <felipepcorral@gmail.com>
2025-07-28 18:11:50 -0300

    Filled the Run method to run a full cycle

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -2,16 +2,43 @@ using System;
 
 class BreathingActivity : Activity
 {
-    private int varName;
-    public BreathingActivity()
+    const string NAME = "Breathing";
+    const string MESSAGE = "This activity will help you relax by walking your through breathing routine. Clear your mind and focus on your breathing.";
+    const int STEP_TIME = 4; //s
+    const int NUM_STEPS = 4; // per cycle
+    public BreathingActivity() : base(NAME, MESSAGE)
     {
     }
 
     public void Run()
     {
+        //initiate the program running it, displaying the messages and getting for how long it should run
+        int time = DisplayStartMessage();
+        //get the next value that creates a complete breathing cycle and set it;
+        time = NextFullCycle(time, STEP_TIME, NUM_STEPS);
+        SetTimer(time);
 
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(time);
+
+        while (DateTime.Now < endTime)
+        {
+            BreatheIn(STEP_TIME);
+            Hold(STEP_TIME);
+            BreatheOut(STEP_TIME);
+            Hold(STEP_TIME);
+        }
     }
 
+    private static int NextFullCycle(int ogTime, int stepTime, int numSteps)
+    {
+        int totalCycleTime = stepTime * numSteps;
+        int remainder = ogTime % totalCycleTime;
+        if (remainder == 0)
+            return ogTime;
+        return ogTime + (totalCycleTime - remainder);
+    }
     public static void BreatheIn(int timeInSeconds)
     {
         List<string> breathIn = new List<string> { " ", ".", "o", "O" };
