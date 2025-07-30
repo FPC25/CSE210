977505faec84ac3ee38eaa9e8c147cc5a5da6998
FPC25 <felipepcorral@gmail.com>
2025-07-30 17:52:38 -0300

    Refactor ListeningActivity class to enhance structure and add prompts for mindfulness reflection

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,17 +1,53 @@
 using System;
 
-class ListeningActivity
+class ListeningActivity : Activity
 {
-    private int varName;
-    public ListeningActivity()
+    const string NAME = "Listing", MESSAGE = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+
+    private List<string> _prompts = new List<string>() {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?",
+        // New questions for mindfulness
+        "What are three things you are grateful for today?",
+        "What are some achievements you are proud of?",
+        "What activities help you feel relaxed?",
+        "What are some places where you feel at peace?",
+        "What are some acts of kindness you have witnessed or performed recently?",
+        "What are some goals you are working towards?",
+        "What are some lessons you have learned this year?",
+        "Who has made a positive impact on your life?",
+        "What are some things you enjoy about your daily routine?",
+        "What are some challenges you have overcome?"
+    };
+    private int _count;
+
+    public ListeningActivity() : base(NAME, MESSAGE)
     {
     }
 
-    public void MethodName()
+    public void Run()
+    {
+        SetTimer(DisplayStartMessage());
+        GetRandomPrompt();
+        Countdown(10);
+        GetListFromUser();
+        DisplayEndMessage();
+    }
+
+    private void GetRandomPrompt()
     {
 
     }
-    public static void Ellipsis(int timeInSeconds)
+
+    private List<string> GetListFromUser()
+    {
+        return new List<string>();
+    }
+
+    public void Ellipsis(int timeInSeconds)
     {
         var ellipsis = new List<string> { ".", ".", "." };
 
