60de51ec1e91010aa76ac752d055c224e7f40ce4
FPC25 <felipepcorral@gmail.com>
2025-07-31 20:31:54 -0300

    Enhance ReflectingActivity class by implementing Run method and adding prompt and question display functionality

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -9,7 +9,8 @@ class ReflectingActivity : Activity
         "Think of a time when you did something really difficult.",
         "Think of a time when you helped someone in need.",
         "Think of a time when you did something truly selfless."
-    }, _questions = new List<string>() {
+    },
+        _questions = new List<string>() {
         "Why was this experience meaningful to you?",
         "Have you ever done anything like this before?",
         "How did you get started?",
@@ -37,8 +38,46 @@ class ReflectingActivity : Activity
 
     public void Run()
     {
-
+        List<string> usedQuestions = new List<string>();
+        int time = DisplayStartMessage();
+        DateTime startime = new DateTime();
+        startime = DateTime.Now;
+        SetTimer(time);
+        DisplayPrompt();
+        DisplayQuestion(startime);
+        DisplayEndMessage();
     }
-    
 
+    private string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    private string GetRandomQuestion(List<string> usedQuestionList)
+    {
+        return "";
+    }
+
+    private void DisplayPrompt()
+    {
+        string prompt;
+        prompt = GetRandomPrompt();
+
+        Console.WriteLine("Considering the following prompt:");
+        Console.WriteLine($"--- {prompt} ---\n");
+
+        Console.WriteLine("When you have something in mind, press Enter to continue.");
+        Console.ReadLine();
+    }
+
+    private void DisplayQuestion(DateTime time)
+    {
+        List<string> usedQuestions = new List<string>();
+        string question;
+        question = GetRandomQuestion(usedQuestions);
+
+        
+        Console.WriteLine("Answer the questions: ");
+        Console.WriteLine(question);
+    }
 }
