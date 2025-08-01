3cb183c10de3393ef5698019b150d8a21b03c0b3
FPC25 <felipepcorral@gmail.com>
2025-08-01 15:39:39 -0300

    Refactoring program with menu logic and running activities

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+
+        const string
+        BREATH = "Start breathing activity",
+        REFLECT = "Start reflecting activity",
+        LIST = "Start listing activity",
+        QUIT = "Quit";
+
+
+        List<string> options = new List<string>()
+        {
+            BREATH,
+            REFLECT,
+            LIST,
+            QUIT
+        };
+
+        Console.WriteLine("Menu Options:");
+        string selectedOption;
+
+        do
+        {
+            selectedOption = Utils.DecisionString(options);
+            switch (selectedOption)
+            {
+                case BREATH:
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.Run();
+                    break;
+
+                case REFLECT:
+                    ReflectingActivity reflecting = new ReflectingActivity();
+                    reflecting.Run();
+                    break;
+
+                case LIST:
+                    //ListingActivity listing = new ListingActivity();
+                    //listing.Run();
+                    Console.WriteLine("Work in Progress");
+                    break;
+
+                case QUIT:
+                    break;
+            }
+        } while (selectedOption != QUIT);
     }
 }
