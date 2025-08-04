d70dbe9ea1a500af32b9be019af5d4e5bbfea1bb
FPC25 <felipepcorral@gmail.com>
2025-08-04 11:39:40 -0300

    Implement main program logic to create and display shapes with area calculations

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square s = new Square(15.2, "blue");
+        Circle c = new Circle(7.5, "red");
+        Rectangle r = new Rectangle(10.0, 5.0, "green");
+
+
+        List<Shape> shapes = new List<Shape>() { s, c, r };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has a area of {shape.GetArea()} u^2");
+        }
     }
 }
