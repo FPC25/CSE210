7e5755de66952f191211c19a886f3ccac866f7d7
FPC25 <felipepcorral@gmail.com>
2025-08-05 15:54:10 -0300

    Add Utils class with utility functions for string manipulation, menu interaction, list printing, and digit counting

---
+++ week06/EternalQuest/Utils.cs
@@ -0,0 +1,62 @@
+using System;
+
+public static class Utils
+{
+    //A function to Capitaliza a string
+    public static string ToTitleCase(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return s;
+        return char.ToUpper(s[0]) + s.Substring(1);
+    }
+
+    // Functions to create a menu from each the user can interact given a list of options
+
+    //returns the option index
+    public static int Decision(List<string> options)
+    {
+        // Display numbered choices
+        for (int i = 0; i < options.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {options[i]}");
+        }
+
+        int choice;
+        string input;
+        // Prompt until user enters a valid number
+        do
+        {
+            Console.Write("Please select an option by number: ");
+            input = Console.ReadLine();
+        } while (!int.TryParse(input, out choice) || choice < 1 || choice > options.Count);
+
+        // Return zero-based index
+        return choice - 1;
+    }
+
+    //returns the text of the option
+    public static string DecisionString(List<string> options)
+    {
+        int index = Decision(options);
+        return options[index];
+    }
+
+    // A function to print all the elements in a list in a python style
+    public static void PrintList(List<string> list)
+    {
+        Console.Write("[");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write($"\"{list[i]}\"");
+            if (i < list.Count - 1)
+                Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+    //Counts the number of digits of a number
+    public static int CountDigit(int number)
+    {
+        if (number < 0) number = Math.Abs(number);
+
+        return number.ToString().Length;
+    }
+}
