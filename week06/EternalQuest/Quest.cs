d8c5dabfa0dd9d4a7fc9da3835a557556b37f342
FPC25 <felipepcorral@gmail.com>
2025-08-07 22:37:10 -0300

    Refactor Quest class to improve constructor and method structure for better clarity and maintainability.

--- week06/EternalQuest/Quest.cs
+++ week06/EternalQuest/Quest.cs
@@ -1,14 +1,41 @@
 using System;
 
-class Quest
+abstract class Quest
 {
-    private int varName;
-    public Quest()
+    private int _xpPoints, _playerXPToNextLevel;
+    private string _shortName, _description, _type;
+    private bool _isCompleted;
+
+    public Quest(string name, string description, string type, int XPNextLevel)
     {
+        _shortName = name;
+        _description = description;
+        _type = type;
+        _playerXPToNextLevel = XPNextLevel;
+        _isCompleted = false;
     }
 
-    public void MethodName()
-    {
+    public abstract void RecordEvent();
 
+    public abstract bool IsComplete();
+
+    public abstract string GetStringRepresentation();
+
+    public abstract int CalculateXpPerQuestType();
+
+    public virtual string GetDetailsString()
+    {
+        string complete, message;
+        if (_isCompleted)
+        {
+            complete = "X";
+        }
+        else
+        {
+            complete = " ";
+        }
+        message = $"[{complete}] {_shortName} - {_description}";
+        return message;
     }
+    
 }
