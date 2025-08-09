7a1ea5794ebb020b339ea6bab7ff4caee08768c7
FPC25 <felipepcorral@gmail.com>
2025-08-09 02:03:45 -0300

    Remove optional parameter from CompleteQuest method for consistency

--- week06/EternalQuest/Quest.cs
+++ week06/EternalQuest/Quest.cs
@@ -76,7 +76,7 @@ abstract class Quest
     /// <summary>
     /// Marks the quest as completed and sets it as inactive.
     /// </summary>
-    protected void CompleteQuest(bool isEternal = false)
+    protected void CompleteQuest()
     {
         _active = false;
         _isCompleted = true;
