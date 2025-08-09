5006bdaa54eb06ef554541c42dfd2fa0fc0de7ae
FPC25 <felipepcorral@gmail.com>
2025-08-09 09:40:17 -0300

    Add initial QuestManager class implementation with constructor

---
+++ week06/EternalQuest/QuestManager.cs
@@ -0,0 +1,15 @@
+using System;
+
+class QuestManager
+{
+    private Profile _player;
+    public QuestManager(Profile player)
+    {
+        _player = player;
+    }
+
+    public void MethodName()
+    {
+        
+    }
+}
