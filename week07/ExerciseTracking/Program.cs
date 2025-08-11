55a3611d5e6e852644c39869839fa84f49c17be0
FPC25 <felipepcorral@gmail.com>
2025-08-11 15:52:34 -0300

    Refactor Main method in Program class for improved clarity and structure

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.Clear();
+        Console.WriteLine("Hello World! This is the ExerciseTracking Project.\n");
+
+        DateTime date = new DateTime(2022, 11, 3);
+        List<Activity> activities = new List<Activity>()
+        {
+            new Running(date, 30, 4.8f),
+            new Cycling(date.AddDays(1), 30, 9.7f),
+            new Swimming(date.AddDays(2), 60, 50),
+            new Running(date.AddDays(3), 45, 7.2f),
+            new Cycling(date.AddDays(4), 60, 20.5f),
+            new Swimming(date.AddDays(5), 30, 25),
+            new Running(date.AddDays(6), 20, 3.0f),
+            new Cycling(date.AddDays(7), 90, 35.0f),
+            new Swimming(date.AddDays(8), 45, 35)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
