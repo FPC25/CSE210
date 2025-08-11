accac56e6dfaaac000bafcb7556ce4bed2b1163b
FPC25 <felipepcorral@gmail.com>
2025-08-11 15:00:14 -0300

    Add Activity class with properties and abstract methods for activity tracking

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,33 @@
+using System;
+
+abstract class Activity
+{
+    private string _name;
+    private DateTime _date;
+    private int _lengthInMinutes;
+
+    public Activity(DateTime date, int length)
+    {
+        _date = date;
+        _lengthInMinutes = length;
+    }
+
+    public DateTime GetDate() => _date;
+
+    public int GetLengthInMinutes() => _lengthInMinutes;
+
+    public string GetName() => _name;
+
+    public void SetName(string name) => _name = name;
+
+    public abstract double GetDistance();
+
+    public abstract double GetSpeed();
+
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} - {_name} ({_lengthInMinutes} min) - Distance: {GetDistance()} km, Speed: {GetSpeed()} km/h, Pace: {GetPace()} min per km";
+    }
+}
